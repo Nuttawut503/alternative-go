c02e2a08adb28a4179890cbace70f0b0d2a0526b
Nuttawut503 <nuttawut.promsuk@gmail.com>
2022-05-01 18:53:35 +0700

    fixed if else

--- algorithms/dynamic/lis.go
+++ algorithms/dynamic/lis.go
@@ -13,13 +13,12 @@ func LongestIncreasingSubsequence(arr []int) int {
 	longestSize := 1
 	for i := 0; i < len(arr)-1; i++ {
 		for j := i + 1; j < len(arr); j++ {
-			if arr[i] < arr[j] {
-				if lis[i]+1 > lis[j] {
-					lis[j] = lis[i] + 1
-					if longestSize < lis[i]+1 {
-						longestSize = lis[i] + 1
-					}
-				}
+			if arr[i] >= arr[j] || lis[i]+1 <= lis[j] {
+				continue
+			}
+			lis[j] = lis[i] + 1
+			if longestSize < lis[j] {
+				longestSize = lis[j]
 			}
 		}
 	}
